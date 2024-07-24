d8dead0b90eddcdf7dee8a40014acd2343d2e243
KatherinaKaplina <156842594+KatherinaKaplina@users.noreply.github.com>
2024-07-24 19:26:57 +0300

    Update test_APIs.py

--- tests/test_APIs.py
+++ tests/test_APIs.py
@@ -15,14 +15,14 @@ def test_add_meme(post_create_meme, data, token):
     post_create_meme.check_info_is_correct(data['info'])
 
 
-@allure.title('Create new meme')
+@allure.title('Create new meme - unauthorized user')
 def test_add_meme_unauthorized(post_create_meme):
     post_create_meme.new_meme(TEST_DATA_CREATE[0], invalid_token)
     post_create_meme.report_errors()
     post_create_meme.check_status_code_is_unauthorized(status_code=401)
 
 
-@allure.title('Create new meme')
+@allure.title('Create new meme - negative checks')
 @pytest.mark.parametrize('data', NEGATIVE_DATA_CREATE)
 def test_add_meme_with_negative_data(post_create_meme, data, token):
     post_create_meme.new_meme(data, token)
@@ -41,7 +41,7 @@ def test_update_meme(new_meme_id, put_update_meme, token):
     put_update_meme.check_info_is_correct(data['info'])
 
 
-@allure.title('Update meme')
+@allure.title('Update meme - unauthorized user')
 def test_update_meme_unauthorized(put_update_meme):
     meme_id = 100
     put_update_meme.make_changes_in_meme(meme_id, TEST_DATA_CREATE[0], invalid_token)
@@ -49,7 +49,7 @@ def test_update_meme_unauthorized(put_update_meme):
     put_update_meme.check_status_code_is_unauthorized(status_code=401)
 
 
-@allure.title('Update meme')
+@allure.title('Update meme - forbidden for another user')
 def test_update_meme_forbidden(put_update_meme):
     meme_id = 100
     put_update_meme.make_changes_in_meme(meme_id, TEST_DATA_CREATE[0], another_user_token)
@@ -57,7 +57,7 @@ def test_update_meme_forbidden(put_update_meme):
     put_update_meme.check_status_code_is_forbidden(status_code=403)
 
 
-@allure.title('Update meme')
+@allure.title('Update meme - negative checks')
 @pytest.mark.parametrize('data', NEGATIVE_DATA_UPDATE)
 def test_update_meme_with_negative_data(put_update_meme, new_meme_id, data, token):
     put_update_meme.make_changes_in_meme(new_meme_id, data, token)
@@ -76,7 +76,7 @@ def test_get_one_meme(new_meme_id, get_request_meme, token):
         get_request_meme.check_info_is_correct(response['info'])
 
 
-@allure.title('Get one meme')
+@allure.title('Get one meme - unauthorized user')
 def test_get_one_meme_unauthorized(get_request_meme):
     meme_id = 100
     get_request_meme.get_meme_by_id(meme_id, invalid_token)
@@ -99,7 +99,7 @@ def test_get_all_meme(get_request_all_memes, token):
             get_request_all_memes.check_info_not_empty(meme)
 
 
-@allure.title('Get all memes')
+@allure.title('Get all memes - unauthorized user')
 def test_get_all_meme_unauthorized(get_request_all_memes):
     get_request_all_memes.get_all_memes(invalid_token)
     get_request_all_memes.report_errors()
@@ -112,7 +112,7 @@ def test_delete_meme(new_meme_id, delete_meme, token):
     delete_meme.check_status_code_is_correct(status_code=200)
 
 
-@allure.title('Delete meme')
+@allure.title('Delete meme - unauthorized user')
 def test_delete_meme_unauthorized(delete_meme):
     meme_id = 100
     delete_meme.delete_meme_by_id(meme_id, invalid_token)
@@ -120,7 +120,7 @@ def test_delete_meme_unauthorized(delete_meme):
     delete_meme.check_status_code_is_unauthorized(status_code=401)
 
 
-@allure.title('Delete meme')
+@allure.title('Delete meme - forbidden for another user')
 def test_delete_meme_forbidden(delete_meme):
     meme_id = 100
     delete_meme.delete_meme_by_id(meme_id, another_user_token)
